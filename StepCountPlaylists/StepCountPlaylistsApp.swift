d66b7ff3c04655d4770b16a6eb91989855be0aeb
Kryvodub Oleksandr <krivodubag@gmail.com>
2024-11-12 08:43:44 +0200

    Project presetup

---
+++ StepCountPlaylists/StepCountPlaylistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCountPlaylistsApp.swift
+//  StepCountPlaylists
+//
+//  Created by Alexander Sharko on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct StepCountPlaylistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
